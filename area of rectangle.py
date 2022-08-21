d94041cee4c9bf89deb0c6751f33a79e7c6ee938
Sagar904 <111702463+Sagar904@users.noreply.github.com>
2022-08-22 00:28:08 +0530

    Create area of rectangle.py

---
+++ area of rectangle.py
@@ -0,0 +1,11 @@
+# python program to find area of rectangle
+
+# take inputs
+length = 2
+width = 5
+
+# calculate area of rectangle
+area = length * width
+
+# display result
+print('Area of rectangle = ',area)

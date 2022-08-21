13ffad83b0d6f4062606d91bd964f308aa83302f
Sagar904 <111702463+Sagar904@users.noreply.github.com>
2022-08-22 00:29:12 +0530

    Create area of the right-angle triangle.py

---
+++ area of the right-angle triangle.py
@@ -0,0 +1,11 @@
+# python program to find the area of the right-angle triangle
+
+# take inputs
+base = float(input('Enter the base of the triangle: '))
+height = float(input('Enter the height of the triangle: '))
+
+# calculate area of triangle
+area = (1/2) * base * height
+
+# display result
+print('Area of triangle = ',area)
